9dc68fb95f0b854681c26a73e1afff35e3452337
mike Baradaran <mike.baradaran@qa.com>
2026-04-20 10:03:26 +0100

    Lab 2 completed

--- src/labs/Program.java
+++ src/labs/Program.java
@@ -1,10 +1,12 @@
 package labs;
 
+import java.io.FileReader;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		System.out.println("Hello World again!");
+	
 	}
 
 }
